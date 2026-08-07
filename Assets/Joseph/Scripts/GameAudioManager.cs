5b21dfc9d13d99d1d83cb71648a9bfab07837686
emjaiyee <mjleguidleguid17@gmail.com>
2026-08-07 19:57:37 +0800

    Setup for global audio manager complete (needs to be called / not yet called by other scripts)

--- Assets/Joseph/Scripts/GameAudioManager.cs
+++ Assets/Joseph/Scripts/GameAudioManager.cs
@@ -2,15 +2,66 @@ using UnityEngine;
 
 public class GameAudioManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    public static GameAudioManager Instance { get; private set; }
+
+    [Header("Audio")]
+    public AudioClip panelOpenSFX;
+    public AudioClip panelCloseSFX;
+    public AudioClip typewriterSFX;
+    public AudioClip roosterSFX;
+    public AudioClip taxPaidSFX;
+    public AudioClip taxFailedSFX;
+    public AudioClip morningAmbienceSFX;
+    private AudioSource sfxSource;
+    private AudioSource ambienceSource;
+    private AudioSource musicSource;
+
+    private void Awake()
     {
-        
+        sfxSource = gameObject.AddComponent<AudioSource>();
+        ambienceSource = gameObject.AddComponent<AudioSource>();
+        musicSource = gameObject.AddComponent<AudioSource>();
+         
+        ambienceSource.loop = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable() => GameEvents.OnPlaySound += PlayAudio;
+    private void OnDisable() => GameEvents.OnPlaySound -= PlayAudio;
+
+    private void PlayAudio(SoundType soundType)
     {
-        
+        switch (soundType)
+        {
+            case SoundType.PanelOpen:
+                sfxSource.PlayOneShot(panelOpenSFX);
+                break;
+            case SoundType.PanelClose:
+                sfxSource.PlayOneShot(panelCloseSFX);
+                break;
+            case SoundType.Typewriter:
+                sfxSource.PlayOneShot(typewriterSFX);
+                break;
+            case SoundType.Rooster:
+                sfxSource.PlayOneShot(roosterSFX);
+                break;
+            case SoundType.TaxPaid:
+                sfxSource.PlayOneShot(taxPaidSFX);
+                break;
+            case SoundType.TaxFailed:
+                sfxSource.PlayOneShot(taxFailedSFX);
+                break;
+            case SoundType.MorningAmbience:
+                PlayAmbience(morningAmbienceSFX);
+                break;
+        }
     }
+
+    private void PlayAmbience(AudioClip clip)
+    {
+        if (ambienceSource.clip == clip && ambienceSource.isPlaying) return;
+        ambienceSource.clip = clip;
+        ambienceSource.Play();
+    }
+
 }
--- Assets/Joseph/Scripts/Player/GameEvent.cs
+++ Assets/Joseph/Scripts/Player/GameEvent.cs
@@ -1,11 +1,35 @@
 using System;
 
+
+public enum SoundType
+{
+    PanelOpen,
+    PanelClose,
+    Typewriter,
+    Rooster,
+    TaxPaid,
+    TaxFailed,
+    MorningAmbience
+}
+
 public static class GameEvents
 {
+
+    
     // Fishing
     public static Action<FishData> OnFishHooked;
     public static Action<ItemData> OnItemCaught;
     public static Action OnFishEscaped;
+
+     public static Action<SoundType> OnPlaySound;
+
+    public static void TriggerSound(SoundType soundType)
+    {
+        OnPlaySound?.Invoke(soundType);
+
+        // TO CALL SOUND USE THIS FUNCTION!
+    // GameEvents.TriggerSound(SoundType.TaxFailed);
+    }
 
     // Inventory
     public static Action<ItemData> OnItemAdded;
